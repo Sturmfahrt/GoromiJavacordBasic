aad7226d04c3f62702cbfae012886ed05e4ea368
max <cyberhead81@icloud.com>
2023-01-27 00:49:16 -0500

    Fucking First commit. for some reason this shit refuses to run. like what the actual fuck.

---
+++ src/main/java/MyFirstBot.java
@@ -0,0 +1,24 @@
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.intent.Intent;
+
+public class MyFirstBot {
+    public static void main(String[] args){
+        System.out.println("I am running and hopefully i will not crash");
+        // Log the bot in
+        DiscordApi api = new DiscordApiBuilder()
+                .setToken("MTA2ODMwNTc2NjE1MjQxMzE4NA.G3Exf1.4lTxpo_qUEwjQW68u2TBBsAZfbr3epVgVrCECg")
+                .addIntents(Intent.MESSAGE_CONTENT)
+                .login().join();
+
+        // Add a listener which answers with "Pong!" if someone writes "!ping"
+        api.addMessageCreateListener(event -> {
+           if (event.getMessageContent().equalsIgnoreCase("!ping")) {
+               event.getChannel().sendMessage("Pong!");
+           }
+        });
+
+        // Print the invite url of your bot
+        System.out.println("You can invite the bot by using the following url: " + api.createBotInvite());
+    }
+}
